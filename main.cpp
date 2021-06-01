9da8e03e4a2e8e4eb092a903e51d22e8163b2184
mercuti0 <83207822+mercuti0@users.noreply.github.com>
2021-05-31 22:34:13 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,59 +1,103 @@
 /* This program calculates the total number of possible different cell phone numbers for a given area code, international country
- * code, and the length of their cell numbers
+ * code, and the length of their cell numbers (after the area and international country code).
  */
 
 #include <iostream>
 #include "console.h"
 #include "testing/SimpleTest.h"
-#include "simpio.h"
 #include "strlib.h"
 using namespace std;
 
-
-void generatePhoneNumbersPR(int length, string soFar, int areaCode, int internationalCountryCode, int firstX, int lastX) {
-    if (length == soFar.size()) {
-        string firstThree = soFar.substr(0, firstX);
-        string lastFour = soFar.substr(firstX, lastX);
-        cout << '+' << internationalCountryCode <<  '(' << areaCode << ')' << firstThree << "-" << lastFour << endl;
+/* This function recursively print all the possible permutations of a phone number.
+ */
+void generatePhoneNumbersPR(int length, string phoneNumber, int areaCode, int internationalCountryCode, int firstX, int lastX, Vector<int>& totalPerm) {
+    // Base case: once the the string has been added the required length of the new phone number, we print it and move on
+    if (length == phoneNumber.size()) {
+        if (phoneNumber.substr(1, 2) != "11") {
+            // Phone numbers in different countries can be formatted a bit differently, hence we separate the two usign the given inputs
+            string startX = phoneNumber.substr(0, firstX);
+            string nextX = phoneNumber.substr(firstX, lastX);
+            // Phone numbers cannot be led by a 0 or 1
+            if (phoneNumber[0] != '0' && phoneNumber[0] != '1') {
+                cout << '+' << internationalCountryCode <<  '(' << areaCode << ")" << startX << "-" << nextX << endl;
+                totalPerm.add(stringToInteger(phoneNumber));
+            }
+        }
     }
     else {
+        // Phone numbers include numbers 0 to 9
         for (int i = 0; i <= 9; i++) {
-            if (soFar[0] != '0' && soFar[0] != '1')
-                generatePhoneNumbersPR(length, soFar + integerToString(i), areaCode, internationalCountryCode, firstX, lastX);
+            // Phone numbers don't start with a leading 0 or 1, or end with 11
+            if (phoneNumber[0] != '0' && phoneNumber[0] != '1')
+                generatePhoneNumbersPR(length, phoneNumber + integerToString(i), areaCode, internationalCountryCode, firstX, lastX, totalPerm);
         }
     }
 }
+
+/* This helper function calcualtes the power of base 10 to the power of the length of phone numbers - 1, giving us the
+ * place (millionth, billionth) to which the permutations can go up to. This function was obtained from Section 2. Big-O
+ * and Recursion.
+ */
 int raiseToPower(int base, int phoneNumberLength) {
     if (phoneNumberLength == 0) return 1;
     return base * raiseToPower(base, phoneNumberLength - 1);
 }
+
+/* This function calculates the total number of permutations of an area code given the length of a phone number.
+ */
 int totalNumbersAvailable(int phoneNumberLength) {
-    int possibilities = raiseToPower(10, phoneNumberLength);
-    cout << "There are " << 8 * (possibilities) << " different phone number possibilities for your area code." << endl;
-    return possibilities;
+    // Calculate the total permutations
+    int possibilities = raiseToPower(10, phoneNumberLength - 1);
+    // Multiply by the total available leading numbers (2-9) and subtract the numbers starting with N11 (80,000).
+    int validPossibilities = (8 * (possibilities)) - 80000;
+    cout << "There are " << validPossibilities << " different phone number possibilities for your area code." << endl;
+    return validPossibilities;
 }
 
-void generatePhoneNumbersPR(int phoneNumberLength, int areaCode, int internationalCountryCode, int firstX, int nextX) {
-    generatePhoneNumbersPR(phoneNumberLength, "", areaCode, internationalCountryCode, firstX, nextX);
+/* This function would be for the user to input their values and have the recursion begin.
+ */
+Vector<int> generatePhoneNumbersPR(int phoneNumberLength, int areaCode, int internationalCountryCode, int firstX, int nextX) {
+    Vector<int> totalPerm;
+    // Overload function and include only the necessary inputs.
+    generatePhoneNumbersPR(phoneNumberLength, "", areaCode, internationalCountryCode, firstX, nextX, totalPerm);
+    // Print total numbers avilable for given phone number length
     totalNumbersAvailable(phoneNumberLength);
+    return totalPerm;
 }
 
-void generatePhoneNumbersI(int length, int areaCode, int internationalCountryCode) {
+/* This function iteratively prints all the possible permutations of a phone number.
+ */
+Vector<int> generatePhoneNumbersI(int length, int areaCode, int internationalCountryCode, int firstX, int nextX) {
+    Vector<int> totalPerm;
     string phoneNumber = "";
+    // Add the total length of the phone number minus the leading first number
     for (int i = 0; i < length - 1; i++) {
         phoneNumber += '0';
     }
+    // We start creating the phone numbers at the lowest possible leading value, 2
+    string topSize = '1' + phoneNumber + '0';
     phoneNumber = '2' + phoneNumber;
-    int totalPerm = totalNumbersAvailable(length);
-    int temp = stringToInteger(phoneNumber);
-    for (int i = 0; i < totalPerm; i ++) {
-        temp ++;
-        phoneNumber = integerToString(temp);
-        string firstThree = phoneNumber.substr(0, 3);
-        string lastFour = phoneNumber.substr(3, 4);
-        cout << '+' << internationalCountryCode <<  '(' << areaCode << ')' << phoneNumber.substr(0, 3)
-             << "-" << phoneNumber.substr(3, 4) << endl;
+    // Calculate the total number of phone numbers available
+    int numberOfPerm = totalNumbersAvailable(length);
+    // If the number of permutations is less than the valid amount, then this is being run for testing purposes.
+    if (numberOfPerm < 0) {
+        numberOfPerm = stringToInteger(topSize);
     }
+    int temp = stringToInteger(phoneNumber);
+    for (int i = 0; i < numberOfPerm; i ++) {
+        // Continue to add the phone number value
+        phoneNumber = integerToString(temp);
+        // Phone numbers in different countries can be formatted a bit differently, hence we separate the two usign the given inputs
+        string firstThree = phoneNumber.substr(0, firstX);
+        string lastFour = phoneNumber.substr(firstX, nextX);
+        // While the second and third value are not 11, we can print the remaining values since we automatically not start at 1 or 0
+        if (phoneNumber.substr(1, 2) != "11" && phoneNumber[0] != '0' && phoneNumber[0] != '1') {
+            cout << '+' << internationalCountryCode <<  '(' << areaCode << ")" << firstThree << "-" << lastFour << endl;
+            totalPerm.add(stringToInteger(phoneNumber));
+        }
+        temp ++;
+    }
+    return totalPerm;
 }
 
 
@@ -68,10 +112,24 @@ int main() {
     return 0;
 }
 
-PROVIDED_TEST("Sample use of SimpleTest")
+PROVIDED_TEST("Test that both the iterative and recursive functions return the same value. Time test each operation.")
 {
-    totalNumbersAvailable(7);
-    generatePhoneNumbersPR(7, "", 956, 1, 3, 4);
-    generatePhoneNumbersBR(7, 956, 1);
-    generatePhoneNumbersI(7, 956, 1);
+//    int usNumbers = totalNumbersAvailable(7);
+//    int swedenNumbers = totalNumbersAvailable(9);
+//    EXPECT_EQUAL(usNumbers, 7920000);
+//    EXPECT_EQUAL(swedenNumbers, 799920000);
+//    EXPECT_EQUAL(generatePhoneNumbersI(1, 1, 1, 1, 0), generatePhoneNumbersPR(1, 1, 1, 1, 0));
+//    EXPECT_EQUAL(generatePhoneNumbersI(2, 1, 1, 2, 0), generatePhoneNumbersPR(2, 1, 1, 2, 0));
+//    EXPECT_EQUAL(generatePhoneNumbersI(3, 1, 1, 3, 0), generatePhoneNumbersPR(3, 1, 1, 3, 0));
+//    TIME_OPERATION(1, generatePhoneNumbersPR(1, 1, 1, 1, 0));
+//    TIME_OPERATION(2, generatePhoneNumbersPR(2, 1, 1, 2, 0));
+//    TIME_OPERATION(3, generatePhoneNumbersPR(3, 1, 1, 3, 0));
+//    TIME_OPERATION(1, generatePhoneNumbersI(1, 1, 1, 1, 0));
+//    TIME_OPERATION(2, generatePhoneNumbersI(2, 1, 1, 2, 0));
+    TIME_OPERATION(5, generatePhoneNumbersPR(5, 1, 1, 3, 2));
+
+//    generatePhoneNumbersPR(3, 956, 1, 3, 0);
+//    generatePhoneNumbersI(3, 956, 1, 3, 0);
+//    generatePhoneNumbersPR(7, 956, 1, 3, 4);
+//    generatePhoneNumbersI(7, 956, 1, 3, 4);
 }
